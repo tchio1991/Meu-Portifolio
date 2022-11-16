b7ff175daf294640f9efc5575a20dba9d0d7e2dd
Thiago Cardoso <109465375+tchio1991@users.noreply.github.com>
2022-11-16 11:02:54 -0300

    Updated Files

--- js/script.js
+++ js/script.js
@@ -19,3 +19,16 @@
         }
         return i;
         }
+
+        /* Barra de progresso vai aumentando de acordo 
+          com a rolagem da pagina 
+          e diminui de acordo com que a pagina suba pra cima */
+
+        window.onscroll = function() {myFunction()};
+
+        function myFunction() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        document.getElementById("progress-bar").style.width = scrolled + "%";
+        }
